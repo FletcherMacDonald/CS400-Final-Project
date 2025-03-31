9fa79a1aa655101209a53d6579e6bf9f3517fddb
Alexis-Rachelle Ramelb <146779949+alexisrachelle@users.noreply.github.com>
2025-03-31 11:44:57 -1000

    analyze_data.py -- alexis' edits
    adding to fletcher's version 1. adding a route to return my medical imaging data

--- analyze_data.py
+++ analyze_data.py
@@ -6,7 +6,9 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
-def read_json() -> list:
+######################################################################
+#fletcher
+def read_ortho_json() -> list:
     """
     Reads the JSON data from the 'ortho.json' file.
     
@@ -21,9 +23,9 @@ def read_json() -> list:
     """
     try:
         with open('ortho.json', 'r') as f:
-            clinic_data = json.load(f)
+            ortho_clinic_data = json.load(f)
             logging.debug("Successfully loaded data from 'ortho.json'.")
-            return clinic_data
+            return ortho_clinic_data
     except FileNotFoundError as e:
         logging.error(f"File not found: {e}")
         return []  # Return an empty list if the file is not found
@@ -33,9 +35,9 @@ def read_json() -> list:
     
 @app.route('/homepage/ortho_data', methods=['GET'])
 def get_clinics():
-    clinic_data = read_json()  #  makes read_json function clinic_data to load the data 
-    if clinic_data:
-        return jsonify(clinic_data)  # return the clinic_data
+    ortho_clinic_data = read_ortho_json()  #  makes read_json function ortho_clinic_data to load the data 
+    if ortho_clinic_data:
+        return jsonify(ortho_clinic_data)  # return the ortho_clinic_data
     else:
         return jsonify({"error": "Unable to load clinic data"}), 500  # if nothing in the clinic_data file return this
     
@@ -43,9 +45,42 @@ def get_clinics():
   #  sorted_clinics = sorted(data, key=lambda clinic: float(clinic.get(google_review_rating, 0) or 0), reverse=True)
    # return sorted_clinics
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5555, debug=True)
-
-
-
+######################################################################
+#alexis
+def read_medical_imaging_json() -> list:
+    """
+    Reads the JSON data from the 'ortho.json' file.
     
+    Description:
+    This function opens 'ortho.json' and loads the clinic data into a Python list.
+    
+    Args:
+    no arguments
+
+    Returns:
+    list: A list of dictionaries representing orthopedic clinics from the JSON file.
+    """
+    try:
+        with open('medical_imaging.json', 'r') as f:
+            medical_imaging_clinic_data = json.load(f)
+            logging.debug("Successfully loaded data from 'ortho.json'.")
+            return medical_imaging_clinic_data
+    except FileNotFoundError as e:
+        logging.error(f"File not found: {e}")
+        return []  # Return an empty list if the file is not found
+    except json.JSONDecodeError as e:
+        logging.error(f"Error decoding JSON: {e}")
+        return []  # Return an empty list if JSON is corrupted
+    
+@app.route('/homepage/medical-imaging_data', methods=['GET'])
+def get_medical_imaging_clinics():
+    medical_imaging_clinic_data = read_medical_imaging_json()  #  makes read_json function ortho_clinic_data to load the data 
+    if medical_imaging_clinic_data:
+        return jsonify(medical_imaging_clinic_data )  # return the ortho_clinic_data
+    else:
+        return jsonify({"error": "Unable to load clinic data"}), 500 
+    
+######################################################################
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
