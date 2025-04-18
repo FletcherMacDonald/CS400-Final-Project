ef796b4d37d29bcf07a85ffbb355dddd8ba3dbf2
alexisrachelle <alexis-rachelle.ramelb@student.chaminade.edu>
2025-04-17 22:34:39 -1000

    tests are not complete. so everything is pau except just need one more pt clinic. after that, i think code would be done done, yayyy.

--- test_analyze_data.py
+++ test_analyze_data.py
@@ -4,7 +4,7 @@ import logging
 
 
 #TESTING OUR THREE GET REQUESTS: #############################################################
-#1st:
+#1:
 def test_get_ortho_clinics():
     response1 = requests.get('http://127.0.0.1:5555/ortho_data')
 
@@ -16,7 +16,8 @@ def test_get_ortho_clinics():
 
     assert response1.json()[0]['clinic_name'] == "All Access Ortho Oahu LLC"
     assert response1.json()[0]['address'] == "1401 South Beretania Street, Suite 102, Honolulu, HI 96814"
-#2nd: 
+
+#2: 
 def test_get_medical_imaging_clinics():
     response2 = requests.get('http://127.0.0.1:5555/medical-imaging_data')
 
@@ -28,7 +29,8 @@ def test_get_medical_imaging_clinics():
 
     assert response2.json()[2]['clinic_name'] == "Invision Imaging -- Medical Arts Building"
     assert response2.json()[2]['address'] == "1010 S King St #109, Honolulu, HI 96814"
-#3rd:
+
+#3:
 def test_get_pt_clinics():
     response2 = requests.get('http://127.0.0.1:5555/pt_data')
 
@@ -41,13 +43,32 @@ def test_get_pt_clinics():
     assert response2.json()[0]['clinic_name'] == "Moon Physical Therapy"
     assert response2.json()[0]['address'] == "95-1057 Ainamakua Dr, Mililani, HI 96789"
 
-
 #TESTING OUR POST REQUEST: #############################################################
-
+#4:
 def test_viewer_input():
-    pass
+    url = "http://127.0.0.1:5555/add-clinic-page"
+    form_data = {
+        "clinic_type": "Orthopedics",
+        "clinic_name": "CS-401 Orthopedics",
+        "address": "3140 Waialae Ave Honolulu, HI 96816",
+        "phone_number": "808-111-1111",
+        "clinic_hours": "Mon-Fri 9am-5pm",
+        "private_or_public": "Private",
+        "google_review_rating": "5",
+        "head_doctor": "Dr. Phil",
+        "date_of_establishment": "2025-01-01"
+    }
+
+    response = requests.post(url, data=form_data, allow_redirects=True)
+
+    assert response.status_code == 200
+    assert "success" in response.text.lower()
 
 #TESTING OUR DELETE REQUEST: #############################################################
-
-def delete_clinic_by_name(clinic_name):         #delete a medical imaging clinic
-    pass
+#5:
+def test_delete_clinic_by_name():
+    clinic_name = "Kuakini Plaza Imaging"
+    url = f"http://127.0.0.1:5555/delete/{clinic_name}"
+    response = requests.delete(url)
+    print(response.status_code)
+    assert response.status_code == 204
